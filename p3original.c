2b82ae085d77be96c1a5904e2caf9bf6612f6015
RNAGESHA <96461787+RNAGESHA@users.noreply.github.com>
2022-01-18 13:23:33 +0000

    p3final.c

---
+++ p3original.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void input(int *a,int *b)
+{
+printf("enter the two number\n");
+scanf("%d %d",a,b );
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("the sum of %d and %d is %d\n",a,b,sum);
+}
+int main()
+{
+  int x,y,sum;
+  input(&x,&y);
+  add(x,y,&sum);
+  output(x,y,sum);
+}
