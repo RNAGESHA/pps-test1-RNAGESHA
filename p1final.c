8090d258a511437d8be131c344e89ec339abd981
RNAGESHA <96461787+RNAGESHA@users.noreply.github.com>
2022-01-26 03:46:57 +0000

    p1final.c

---
+++ p1final.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  printf("enter the two nombers\n");
+  scanf("%d %d",&a,&b);
+  sum = a + b;
+  printf("sum of %d and %d is %d",a,b,sum);
+  return 0;
+}
